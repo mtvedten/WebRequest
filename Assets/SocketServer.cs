80847e82ee47e71e035091ae9fc8b550adca758e
mtvedten <morten@mediafarm.no>
2017-03-27 21:57:39 +0200

    Første commit
    Test på sockets og web request

---
+++ Assets/SocketServer.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System;
+using UnityEngine.UI; 
+
+
+public class SocketServer : MonoBehaviour {
+
+    public Text myText;
+
+	// Use this for initialization
+	void Start () {
+
+      
+      myText.text = SocketSendReceive("sjernaroy.tvedten.com", 8086);
+    }
+
+
+    // This method requests the home page content for the specified server.
+    private static string SocketSendReceive(string server, int port)
+    {
+        string request = "GET / HTTP/1.1\r\nHost: " + server +
+            "\r\nConnection: Close\r\n\r\n";
+        Byte[] bytesSent = Encoding.ASCII.GetBytes(request);
+        Byte[] bytesReceived = new Byte[256];
+
+        // Create a socket connection with the specified server and port.
+        Socket s = ConnectSocket(server, port);
+
+        if (s == null)
+            return ("Connection failed");
+
+        // Send request to the server.
+        s.Send(bytesSent, bytesSent.Length, 0);
+     
+
+        // Receive the server home page content.
+        int bytes = 0;
+        string page = "Default HTML page on " + server + ":\r\n";
+
+        // The following will block until te page is transmitted.
+        do
+        {
+            bytes = s.Receive(bytesReceived, bytesReceived.Length, 0);
+            page = page + Encoding.ASCII.GetString(bytesReceived, 0, bytes);
+        }
+        while (bytes > 0);
+
+        return page;
+    }
+
+
+
+
+    private static Socket ConnectSocket(string server, int port)
+    {
+        Socket s = null;
+        IPHostEntry hostEntry = null;
+
+        // Get host related information.
+        hostEntry = Dns.GetHostEntry(server);
+
+        // Loop through the AddressList to obtain the supported AddressFamily. This is to avoid
+        // an exception that occurs when the host IP Address is not compatible with the address family
+        // (typical in the IPv6 case).
+        foreach (IPAddress address in hostEntry.AddressList)
+        {
+            IPEndPoint ipe = new IPEndPoint(address, port);
+            Socket tempSocket =
+                new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            tempSocket.Connect(ipe);
+
+            if (tempSocket.Connected)
+            {
+                s = tempSocket;
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+        return s;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/WebRequest.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Xml;
+
+class WebRequest : MonoBehaviour
+{
+
+    private XmlDocument xmlDoc;
+    private WWW www;
+    private TextAsset textXml;
+    private List<string> Players;
+    private string fileName;
+
+    void Start()
+    {
+    //  StartCoroutine(GetText());
+   
+      //  string myXML = "<?xml version = \"1.0\" encoding=\"UTF - 8\" ?>  < tempdata numDevices = \"3\" > < tempsensor id = \"0\" > 5.44 </ tempsensor > < tempsensor id = \"1\" > 5.19 </ tempsensor >  < tempsensor id = \"2\" > 3.88 </ tempsensor >  < relay status = \"0\" />     </ tempdata > ";
+      GetXML();
+
+    }
+
+
+
+   void GetXML()
+    {
+        XmlReader xmlReader = XmlReader.Create("http://sjernaroy.tvedten.com:8086/", new XmlReaderSettings { IgnoreWhitespace = true });
+        //   xmlReader.ReadInnerXml();
+
+        //  XmlReader xmlReader = XmlReader.Create(xml);
+
+
+        xmlReader.MoveToContent();
+       xmlReader.Read();
+      
+        while (xmlReader.Name == "tempsensor")
+        {
+            Debug.Log(xmlReader.GetAttribute("id") + " : " + xmlReader.ReadInnerXml());
+
+        }
+
+     //   xmlReader.Read();
+        
+           
+            Debug.Log("Relay: " + xmlReader.GetAttribute("status"));
+
+      //  }
+        //        Debug.Log(xmlReader.GetAttribute("id")  + " : " + xmlReader.ReadInnerXml());
+        //        Debug.Log(xmlReader.GetAttribute("id") + " : " + xmlReader.ReadInnerXml());
+        // xmlReader.MoveToElement();
+        //Debug.Log("Element: " + xmlReader.);
+        // while (xmlReader.Read())
+        //{
+
+        // Debug.Log(xmlReader.NodeType + " " + xmlReader.Name);
+        //Debug.Log(xmlReader.Name + " - " + xmlReader.Value);
+        if ((xmlReader.NodeType == XmlNodeType.Element) && (xmlReader.Name == "tempsensor"))
+
+            {
+
+             //    Debug.Log("Text: " + xmlReader.ReadInnerXml());
+                
+               // Debug.Log(xmlReader.GetAttribute("id") + ": " + xmlReader.GetAttribute("temperature") + " " + );
+
+            }
+        //}
+
+    }
+
+
+    IEnumerator GetText()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("http://sjernaroy.tvedten.com:8086/");
+        yield return www.Send();
+
+
+
+        if (www.isError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+          //  GetXML(www.downloadHandler.text);
+
+           // xmlDoc.LoadXml(www.downloadHandler.text);
+           // Or retrieve results as binary data
+           // Debug.Log(xmlDoc);
+            byte[] results = www.downloadHandler.data;
+           // readXml();
+        }
+    }
+
+
+    // Following method reads the xml file and display its content 
+    private void readXml()
+    {
+        foreach (XmlElement node in xmlDoc.SelectNodes("tempdata/tempsensor"))
+        {
+
+            Debug.Log(node.GetAttribute("id"));
+            Debug.Log(node.SelectSingleNode("tempsensor ").InnerText);
+
+            /*
+            Player tempPlayer = new Player();
+
+            tempPlayer.Id = int.Parse(node.GetAttribute("id"));
+            tempPlayer.name = node.SelectSingleNode("name").InnerText;
+            tempPlayer.score = int.Parse(node.SelectSingleNode("score").InnerText);
+            Players.Add(tempPlayer); displayPlayeData(tempPlayer);
+            */
+        }
+
+        //- See more at: http://www.theappguruz.com/blog/unity-xml-parsing-unity#sthash.Bs6scgLo.dpuf
+    }
+}
+/*
+ <?xml version = "1.0" encoding="UTF-8"?>
+<tempdata numDevices = "3" >
+    < tempsensor id="0">5.44</tempsensor>
+    <tempsensor id = "1" > 5.19 </ tempsensor >
+    < tempsensor id="2">3.88</tempsensor>
+    <relay status = "0" />
+ </ tempdata >
+*/
