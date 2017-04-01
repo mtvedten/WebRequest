90e05ccc64796e9df202b9b65eb8a6826b1329a9
mtvedten <morten@mediafarm.no>
2017-04-02 01:50:05 +0200

    Working Socket

--- Assets/SocketServer.cs
+++ Assets/SocketServer.cs
@@ -13,32 +13,69 @@ public class SocketServer : MonoBehaviour {
 
     public Text myText;
 
-	// Use this for initialization
-	void Start () {
+    private string server = "192.168.1.120";
+    private int port;
+    // Use this for initialization
+    void Start () {
 
-      
-      myText.text = SocketSendReceive("sjernaroy.tvedten.com", 8086);
+
+        // myText.text = SocketSendReceive("sjernaroy.tvedten.com", 8086);
+        myText.text = SocketSendReceive("192.168.1.177", 80);
+        // Socket s = ConnectSocket("192.168.1.120", 80);
+
+        //myText.text =  sendCommand("S",s);
     }
 
-
-    // This method requests the home page content for the specified server.
-    private static string SocketSendReceive(string server, int port)
+    private string sendCommand(string str, Socket s)
     {
-        string request = "GET / HTTP/1.1\r\nHost: " + server +
-            "\r\nConnection: Close\r\n\r\n";
-        Byte[] bytesSent = Encoding.ASCII.GetBytes(request);
+        
+        Byte[] bytesSent = Encoding.ASCII.GetBytes(str);
         Byte[] bytesReceived = new Byte[256];
 
         // Create a socket connection with the specified server and port.
-        Socket s = ConnectSocket(server, port);
+        //Socket s = ConnectSocket(server, port);
 
         if (s == null)
             return ("Connection failed");
 
         // Send request to the server.
         s.Send(bytesSent, bytesSent.Length, 0);
-     
 
+
+        // Receive the server home page content.
+        int bytes = 0;
+        string page = "Default HTML page on " + server + ":\r\n";
+
+        // The following will block until te page is transmitted.
+       
+            do
+            {
+                bytes = s.Receive(bytesReceived, bytesReceived.Length, 0);
+                page = page + Encoding.ASCII.GetString(bytesReceived, 0, bytes);
+            }
+            while (bytes > 0);
+    
+        return page;
+
+    }
+
+    // This method requests the home page content for the specified server.
+    private static string SocketSendReceive(string server, int port)
+    {
+        string request = "GET / HTTP/1.1\r\nHost: " + server +  "\r\nConnection: Close\r\n\r\n";
+        Byte[] bytesSent = Encoding.ASCII.GetBytes(request);
+        Byte[] bytesReceived = new Byte[256];
+
+        // Create a socket connection with the specified server and port.
+        Socket s = ConnectSocket(server, port);
+        
+        if (s == null)
+            return ("Connection failed");
+
+        // Send request to the server.
+        s.Send(bytesSent, bytesSent.Length, 0);
+     
+       
         // Receive the server home page content.
         int bytes = 0;
         string page = "Default HTML page on " + server + ":\r\n";
@@ -54,7 +91,7 @@ public class SocketServer : MonoBehaviour {
         return page;
     }
 
-
+    
 
 
     private static Socket ConnectSocket(string server, int port)
@@ -79,10 +116,12 @@ public class SocketServer : MonoBehaviour {
             if (tempSocket.Connected)
             {
                 s = tempSocket;
+                Debug.Log("Socket connected");
                 break;
             }
             else
             {
+                Debug.Log("Socket NOT connected");
                 continue;
             }
         }
